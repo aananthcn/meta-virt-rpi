b67be2519b8f7de5985f17ce589c4fe06f9a83bc
Aananth C N <c.n.aananth@gmail.com>
2021-05-25 12:46:52 +0530

    Fixed minor typos in dom0-startup and added a setup script.

--- dom0-startup/files/dom0-startup.sh
+++ dom0-startup/files/dom0-startup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Author: Aananth C N
+# Date: 23 May 2021, 10:23 PM
 
 ### BEGIN INIT INFO
 # Provides: dom0-startup.sh
@@ -7,14 +9,13 @@
 # Default-Start: 2 3 4 5
 # Default-Stop: 0 1 6
 # Short-Description: Starts the dom0 startup script
-# Description: This script will do the things that are to be done
-#              at startup.
+# Description: This is a startup script for dom0. User can add what they want here!
 ### END INIT INFO
 
 # Get function from functions library
 . /etc/init.d/functions
 
-# Start the service FOO
+# Start the service dom0-startup.sh
 start() {
         initlog -c "echo -n Starting dom0 startup: "
 
@@ -26,7 +27,7 @@ start() {
         echo
 }
 
-# Restart the service FOO
+# Restart the service dom0-startup.sh
 stop() {
         initlog -c "echo -n Stopping dom0 startup: "
         echo
@@ -41,7 +42,7 @@ case "$1" in
         stop
         ;;
   status)
-        status FOO
+        status dom0-startup.sh
         ;;
   restart|reload|condrestart)
         stop
---
+++ setup-yocto.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+# Author: Aananth C N
+# Date: 25 May 2021, 10:25 AM
+# This script will setup yocto (i.e., setup layers and conf files) for Xen
+# Virtualization development on Raspberry Pi 4.
+
+# Macros
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "$SCRIPT")
+WORKDIR=$(dirname "$SCRIPTDIR")
+TOPDIRPRINT='BBPATH = "${TOPDIR}"'
+echo "Current directory: $SCRIPTDIR"
+
+# Functions
+copy_bblayer_conf() {
+    cat > conf/bblayers.conf <<EOF
+# POKY_BBLAYERS_CONF_VERSION is increased each time build/conf/bblayers.conf
+# changes incompatibly
+POKY_BBLAYERS_CONF_VERSION = "2"
+
+${TOPDIRPRINT}
+BBFILES ?= ""
+
+BBLAYERS ?= " \
+  ${WORKDIR}/poky/meta \
+  ${WORKDIR}/poky/meta-poky \
+  ${WORKDIR}/poky/meta-yocto-bsp \
+  ${WORKDIR}/meta-openembedded/meta-oe \
+  ${WORKDIR}/meta-openembedded/meta-filesystems \
+  ${WORKDIR}/meta-openembedded/meta-python \
+  ${WORKDIR}/meta-openembedded/meta-networking \
+  ${WORKDIR}/meta-virtualization \
+  ${WORKDIR}/meta-raspberrypi \
+  ${WORKDIR}/meta-virt-rpi \
+  "
+EOF
+}
+
+create_local_conf() {
+    cat > conf/local.conf <<EOF
+MACHINE ?= "raspberrypi4-64"
+DISTRO_FEATURES += " virtualization xen" 
+DL_DIR = "/opt/dl-dir"
+EOF
+}
+
+# Instructions - Clone Layers
+echo "Changing directory to $WORKDIR"
+cd ${WORKDIR}
+if [ ! -d poky ]; then
+    git clone --depth 1 -b dunfell http://git.yoctoproject.org/git/poky
+fi
+
+if [ ! -d meta-raspberrypi ]; then
+    git clone --depth 1 -b dunfell git://git.yoctoproject.org/meta-raspberrypi
+fi
+
+if [ ! -d meta-openembedded ]; then
+    git clone --depth 1 -b dunfell git://git.openembedded.org/meta-openembedded
+fi
+
+if [ ! -d meta-virtualization ]; then
+    git clone --depth 1 -b dunfell https://git.yoctoproject.org/git/meta-virtualization
+fi
+
+# Setup build
+if [ ! -d build ]; then
+    source poky/oe-init-build-env
+    copy_bblayer_conf
+    create_local_conf
+fi
+
+# Final message to developers
+echo ""
+echo "Now run the following command:"
+echo "------------------------------"
+echo "$ cd ${WORKDIR}"
+echo "$ source poky/oe-init-build-env"
+echo "$ bitbake dom0-image"
+echo ""
